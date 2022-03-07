742bbada454e2a27430401cac2b9f80c8e0e6546
Mike Owino <mike@lancolatech.co.ke>
2022-03-07 14:18:27 +0300

    Site live again

--- components/shutdownNotice/shutdownNotice.js
+++ components/shutdownNotice/shutdownNotice.js
@@ -10,7 +10,7 @@ export default function shutdownNotice({ close }) {
         <div className={classes.warningContainer}>
           <PowerSettingsNewIcon className={ classes.warningIcon } />
           <Typography className={classes.para2} align='center'>
-            Site live agein.
+            Site live again.
           </Typography>
           <Typography className={classes.para2} align='center'>
             Few changes happening in the website you might encounter some few bugs in the next few days.
