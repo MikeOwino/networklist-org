d7d54c925b712a6f1789b5f03474673821ca48e2
mikey <mike.owino40@yahoo.com>
2022-06-02 06:36:31 +0000

    createTheme

--- theme/light.js
+++ theme/light.js
@@ -1,4 +1,4 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import coreTheme from './coreTheme';
 
